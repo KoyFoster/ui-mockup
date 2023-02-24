0bca6da9b24190cdab9fc3a3cda5f22f916bb47a
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2023-02-23 20:53:03 -0600

    Started menu

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -19,6 +19,21 @@ export function App() {
           <div>Mary</div>
         </div>
       </div>
+      <div className="menu">
+        <div className="options">
+          <div id="Portrait">[Portrait]</div>
+
+          <div id="Attack">[Attack]</div>
+          <div id="Energy">[Energy]</div>
+          <div id="Djinn">[Djinn]</div>
+          <div id="Summon">[Summon]</div>
+          <div id="Defend">[Defend]</div>
+
+          <div id="Menu-Focused">
+            <div>[Menu-Focused]</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
