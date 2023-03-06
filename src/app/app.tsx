d62638d7bb5dcc85cf93a3255cc39d9e4553a46b
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2023-03-05 19:22:21 -0600

    Reworked menu to be able to branch and navigate

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,48 +3,87 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.scss';
 import './app.scss';
 import menuSheet from '../assets/sprites/Golden-Sun-Menu-Assets.png';
+import { cloneDeep } from 'lodash';
 
 /* Globals */
 interface MenuItem {
+  id: string;
   label: string;
   map: [number, number, number, number];
   sprite: string;
 }
+
+//
 const Menu = {
-  keys: ['attack', 'energy', 'djinn', 'summon', 'items', 'defend'],
-  attack: {
-    label: 'Attack',
-    map: [12, 134, 24, 24],
+  keys: ['fight', 'run', 'status'],
+  loaded: false,
+  fight: {
+    id: 'fight',
+    label: 'Fight',
+    map: [12, 106, 24, 24],
     sprite: '',
+
+    menu: {
+      keys: ['attack', 'energy', 'djinn', 'summon', 'items', 'defend'],
+      loaded: false,
+      attack: {
+        id: 'attack',
+        label: 'Attack',
+        map: [12, 134, 24, 24],
+        sprite: '',
+      },
+      energy: {
+        id: 'energy',
+        label: 'Energy',
+        map: [36, 134, 24, 24],
+        sprite: '',
+      },
+      djinn: {
+        id: 'djinn',
+        label: 'Djinn',
+        map: [60, 134, 24, 24],
+        sprite: '',
+      },
+      summon: {
+        id: 'summon',
+        label: 'Summon',
+        map: [84, 134, 24, 24],
+        sprite: '',
+      },
+      items: {
+        id: 'items',
+        label: 'Items',
+        map: [108, 134, 24, 24],
+        sprite: '',
+      },
+      defend: {
+        id: 'defend',
+        label: 'Defend',
+        map: [132, 134, 24, 24],
+        sprite: '',
+      },
+    },
   },
-  energy: {
-    label: 'Energy',
-    map: [36, 134, 24, 24],
+
+  run: {
+    id: 'run',
+    label: 'Run',
+    map: [36, 106, 24, 24],
     sprite: '',
+    action: () => {
+      console.log('コイたち逃げ出した');
+    },
   },
-  djinn: {
-    label: 'Djinn',
-    map: [60, 134, 24, 24],
-    sprite: '',
-  },
-  summon: {
-    label: 'Summon',
-    map: [84, 134, 24, 24],
-    sprite: '',
-  },
-  items: {
-    label: 'Items',
-    map: [108, 134, 24, 24],
-    sprite: '',
-  },
-  defend: {
-    label: 'Defend',
-    map: [132, 134, 24, 24],
+
+  status: {
+    id: 'status',
+    label: 'Status',
+    map: [60, 106, 24, 24],
     sprite: '',
   },
 } as {
-  [key: string]: MenuItem | string[];
-  keys: string[];
+  [key: string]: MenuItem | string[] | any;
+  // keys: string[];
 };
 
 function loadImage(url: string) {
@@ -59,7 +98,7 @@ function loadImage(url: string) {
 async function getSprite(map: [number, number, number, number]) {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const img = (await loadImage(menuSheet)) as CanvasImageSource;
-  
+
   const ctx = canvas?.getContext('2d');
   canvas.width = map[2];
   canvas.height = map[3];
@@ -71,29 +110,75 @@ async function getSprite(map: [number, number, number, number]) {
 }
 
 export function App() {
-  const [selected, setSelected] = useState('');
+  const [actionMenu, setActionMenu] = useState(cloneDeep(Menu));
+  const [menuPosition, setMenuPosition] = useState({
+    context: actionMenu,
+    path: '',
+  });
   const [loaded, setLoaded] = useState(false);
-  const [actionMenu, setActionMenu] = useState(structuredClone(Menu));
 
   const loadSprites = () => {
+    const menu = menuPosition.context;
+    if (!menu.isLoaded) {
+      setLoaded(false);
+      console.log('loading sprites');
+      menu?.keys.forEach(async (key: string, i: number) => {
+        const item = menu[key] as MenuItem;
+        if (!item.sprite && item.map) item.sprite = await getSprite(item.map);
 
-  actionMenu.keys.forEach(async (key: string, i) => {
-    const item = actionMenu[key] as MenuItem;
-    if (!item.sprite && item.map) item.sprite = await getSprite(item.map);
-
-    if(i+1 === actionMenu.keys.length)
-    {      
-      setActionMenu(actionMenu);
-      setLoaded(true);
+        if (i + 1 === menu.keys.length) {
+          menu[key].isLoaded = true;
+          setActionMenu(actionMenu);
+          setLoaded(true);
+          console.log('sprites loaded');
+        }
+      });
     }
-  });
-
-  // Update State
-};
+    // Update State
+  };
 
   useEffect(() => {
     loadSprites();
-  }, []);
+  }, [menuPosition?.context]);
+
+  function getMenu() {
+    return menuPosition.context?.keys.map((key: string) => {
+      const item = menuPosition.context[key] as MenuItem;
+      const { id: action } = item;
+
+      // if(!item.sprite && item.map) item.sprite = await getSprite(item.map);
+
+      return (
+        <button
+          type="button"
+          className="menu-actions"
+          key={key}
+          // id={menuPosition[0] === action ? 'active' : 'inactive'}
+          onClick={() => {
+            // Enter menu context
+            if (menuPosition.context[action].menu) {
+              setMenuPosition({
+                context: menuPosition.context[action].menu,
+                path: '',
+              });
+            }
+            // Run Behavior
+            else {
+              if (menuPosition.context[action].action)
+                menuPosition.context[action].action();
+              else console.log(`You tried to ${action}, but nothing happened.`);
+            }
+          }}
+          draggable="false"
+          data-tool-tip={`${action}`}
+        >
+          {!item.sprite || (
+            <img src={item.sprite} alt={action} draggable="false" />
+          )}
+        </button>
+      );
+    });
+  }
 
   return (
     <div className="App">
@@ -120,35 +205,14 @@ export function App() {
             </div>
           </div>
           <div className="menu">
-              <img id="Portrait" src='../assets/sprites/portrait.jpg' alt='Portrait' />
-              <div id="Gap" />
-              <div className="actions">
-                {actionMenu.keys.map((key: string) => {
-                  const item = actionMenu[key] as MenuItem;
-                  const { label: action } = item;
-
-                  // if(!item.sprite && item.map) item.sprite = await getSprite(item.map);
-
-                  return (
-                    <button
-                      type="button"
-                      className="menu-actions"
-                      key={key}
-                      id={selected === action ? 'active' : 'inactive'}
-                      onClick={() => {
-                        setSelected(action);
-                      }}
-                      draggable="false"
-                      data-tool-tip={`${action}`}
-                    >
-                      {!item.sprite || (
-                        <img src={item.sprite} alt={action} draggable="false" />
-                      )}
-                    </button>
-                  );
-                })}
-              </div>
-              <div className="menu-frame" id={selected} />
+            <img
+              id="Portrait"
+              src="../assets/sprites/portrait.jpg"
+              alt="Portrait"
+            />
+            <div id="Gap" />
+            <div className="actions">{getMenu()}</div>
+            <div className="menu-frame" id={menuPosition.context.id} />
           </div>
         </>
       )}
