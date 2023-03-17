018d2ad233911d7311b2edafc1b42691ac6e0f98
KoyFoster <12465882+KoyFoster@users.noreply.github.com>
2023-03-16 19:25:43 -0500

    Started planning out menu select

--- src/app/golden-sun/menus/encounter/sub-menus/energy/index.tsx
+++ src/app/golden-sun/menus/encounter/sub-menus/energy/index.tsx
@@ -15,7 +15,14 @@ const Energy = () => {
 
   return (
     <div className="battle-menu energy-menu">
-      <div ref={ref} className="menu-frame menu-options-menu" />
+      <div ref={ref} className="menu-frame menu-options-menu">
+        <div className='page-nav'></div>
+        <ul>
+          <li><button>Coffee</button></li>
+          <li><button>Tea</button></li>
+          <li><button>Coca Cola</button></li>
+        </ul>
+      </div>
     </div>
   );
 };
